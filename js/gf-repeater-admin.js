83469818e0d557519a76e5b3e6e16af3b0cb2d01
Kodie Grantham <kodie.grantham@gmail.com>
2016-02-18 14:35:40 -0600

    Added valid repeater field checking in form editor

--- js/gf-repeater-admin.js
+++ js/gf-repeater-admin.js
@@ -1,4 +1,4 @@
-var gfRepeater_debug = true;
+var gfRepeater_debug = false;
 var gfRepeater_repeaters = {};
 var gfRepeater_page = gf_repeater_js_admin_strings.page;
 
@@ -8,6 +8,7 @@ function gfRepeater_editforms_getRepeaters() {
 	var repeaterChildren = [];
 	var repeaterChildrenIds = [];
 	var repeaterStartId;
+	var repeaterEndId;
 
 	jQuery('.gfield').each(function(){
 		if (repeaterFound == 0) {
@@ -21,10 +22,14 @@ function gfRepeater_editforms_getRepeaters() {
 			if (jQuery(this).has('.gf-repeater-start').length) { return false; }
 			if (jQuery(this).has('.gf-repeater-end').length) {
 				if (gfRepeater_debug) { console.log('Repeater #'+repeaterId+' - End: '+jQuery(this).attr('id')); }
-				gfRepeater_repeaters[repeaterId] = {startId:repeaterStartId,childrenIds:repeaterChildrenIds,children:repeaterChildren};
+				repeaterEndId = this.id;
+				gfRepeater_repeaters[repeaterId] = {startId:repeaterStartId,endId:repeaterEndId,childrenIds:repeaterChildrenIds,children:repeaterChildren};
+
 				repeaterChildren = [];
 				repeaterChildrenIds = [];
 				repeaterFound = 0;
+				repeaterStartId = null;
+				repeaterEndId = null;
 			} else {
 				repeaterChildren.push(this);
 				repeaterChildrenIds.push(this.id);
@@ -46,13 +51,130 @@ function gfRepeater_getField(id) {
 	return GetFieldById(idNum);
 }
 
-function gfRepeater_editforms_getRepeaterId(elementId) {
-	jQuery.each(gfRepeater_repeaters, function(key, value){
-		if (jQuery.inArray(elementId, this['childrenIds'])) {
-			return key;
+function gfRepeater_getIndex(id) {
+	var idFound = false;
+	var x = -1;
+
+	jQuery('.gfield').each(function(){
+		x += 1;
+
+		if (this.id == id) {
+			idFound = true;
+			return false;
 		}
 	});
-	return false;
+
+	if (idFound) {
+		return x;
+	} else {
+		return false;
+	}
+}
+
+function gfRepeater_getRepeaterEndId(id) {
+	var pointerInRepeater = false;
+	var repeaterEndId;
+
+	jQuery('.gfield').each(function(){
+		var field = gfRepeater_getField(this.id);
+		var fieldId = gfRepeater_getId(this.id);
+
+		if (!pointerInRepeater) {
+			if (field.type == 'repeater' && fieldId == id) {
+				pointerInRepeater = true;
+			}
+		} else {
+			if (field.type == 'repeater-end') {
+				repeaterEndId = this.id;
+				pointerInRepeater = false;
+			}
+		}
+	});
+
+	if (repeaterEndId) {
+		return repeaterEndId;
+	} else {
+		return false;
+	}
+}
+
+function gfRepeater_isIndexInsideRepeater(index) {
+	var pointerInRepeater = false;
+	var indexInRepeater = false;
+	var x = -1;
+
+	jQuery('.gfield').each(function(){
+		x += 1;
+		var field = gfRepeater_getField(this.id);
+
+		if (!pointerInRepeater) {
+			if (field.type == 'repeater') {
+				if (gfRepeater_debug) { console.log('Repeater Start Index: ' + x); }
+				pointerInRepeater = true;
+			}
+		} else {
+			if (field.type == 'repeater-end') {
+				if (gfRepeater_debug) { console.log('Repeater End Index: ' + x); }
+				pointerInRepeater = false;
+			} else {
+				if (gfRepeater_debug) { console.log('Repeater Child Index: ' + x); }
+			}
+
+			if (x == index) {
+				indexInRepeater = true;
+			}
+		}
+	});
+
+	if (pointerInRepeater) {
+		indexInRepeater = false;
+	}
+
+	if (gfRepeater_debug) { console.log('Is the index ' + index + ' inside of a repeater? ' + indexInRepeater); }
+
+	return indexInRepeater;
+}
+
+function gfRepeater_brokenRepeater() {
+	var pointerInRepeater = false;
+	var brokenRepeater = false;
+	var x = -1;
+
+	jQuery('.gfield').each(function(){
+		x += 1;
+		var field = gfRepeater_getField(this.id);
+
+		if (!pointerInRepeater) {
+			if (field.type == 'repeater') {
+				pointerInRepeater = true;
+			} else if (field.type == 'repeater-end') {
+				alert('One of your repeaters are missing their repeater start field!');
+				brokenRepeater = true;
+				return false;
+			}
+		} else {
+			if (field.type == 'repeater') {
+				alert('Nesting Repeaters is not supported!');
+				brokenRepeater = true;
+				return false;
+			} else if (field.type == 'repeater-end') {
+				pointerInRepeater = false;
+			} else {
+				var fieldCheck = gfRepeater_editforms_fieldCheck(field.type, x);
+				if (!fieldCheck) {
+					brokenRepeater = true;
+					return false;
+				}
+			}
+		}
+	});
+
+	if (!brokenRepeater && pointerInRepeater) {
+		alert('One of your repeaters are missing their repeater end field!');
+		brokenRepeater = true;
+	}
+
+	return brokenRepeater;
 }
 
 function gfRepeater_editforms_update(leaving) {
@@ -116,18 +238,127 @@ function gfRepeater_editforms_updateRequired(field, leaving) {
 	return returnRequired;
 }
 
-function gfRepeater_editforms_updateUpdateButton() {
-	var updateButton = jQuery('#floatMenu input.update-form');
-	var onClickEvent = updateButton.attr('onclick');
-	var addClickEvent = 'gfRepeater_editforms_update(true);';
-	var newClickEvent = addClickEvent+onClickEvent;
-	updateButton.attr('onclick', newClickEvent);
+function gfRepeater_editforms_fieldCheck(type, index) {
+	var supportedTypes = ['text', 'textarea', 'select', 'multiselect', 'number',
+							'checkbox', 'radio', 'hidden', 'html', 'section',
+							'name', 'date', 'time', 'phone', 'address',
+							'website', 'email', 'product', 'quanity', 'option',
+							'shipping', 'total'];
+	
+	if (gfRepeater_isIndexInsideRepeater(index)) {
+		if (type == 'repeater') {
+			alert('Nesting repeaters is not supported!');
+			return false;
+		} else if (type !== 'repeater-end' && jQuery.inArray(type, supportedTypes) == -1) {
+			alert('Sorry, the ' + type + ' field is not supported by the Gravity Forms Repeater Add-On at this time. :(');
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function gfRepeater_editforms_patch_SaveForm() {
+	var og_SaveForm = SaveForm;
+
+	SaveForm = function(isNew) {
+		if (gfRepeater_brokenRepeater()) {
+			return false;
+		}
+
+		gfRepeater_editforms_update(true);
+
+		return og_SaveForm.call(this, isNew);
+	}
+}
+
+function gfRepeater_editforms_patch_StartAddField() {
+	var og_StartAddField = StartAddField;
+
+	StartAddField = function(type, index) {
+		var keepGoing = gfRepeater_editforms_fieldCheck(type, index);
+
+		if (!keepGoing) {
+			jQuery('#gform_adding_field_spinner').remove();
+			return;
+		}
+
+		return og_StartAddField.call(this, type, index);
+	};
+}
+
+function gfRepeater_editforms_patch_EndAddField() {
+	var og_EndAddField = EndAddField;
+
+	EndAddField = function(field, fieldString, index) {
+		og_EndAddField.call(this, field, fieldString, index);
+
+		if (field.type == 'repeater') {
+			StartAddField('repeater-end', index + 1);
+		}
+	};
+}
+
+function gfRepeater_editforms_patch_StartDeleteField() {
+	var og_StartDeleteField = StartDeleteField;
+
+	StartDeleteField = function(element) {
+		var fieldId = jQuery(element)[0].id.split("_")[2];
+		var field = GetFieldById(fieldId);
+
+		if (field.type == 'repeater-end') {
+			alert('This field cannot be deleted directly. Please delete the repeater start field first.');
+			return;
+		}
+
+		og_StartDeleteField.call(this, element);
+	};
+}
+
+function gfRepeater_editforms_patch_EndDeleteField() {
+	var og_EndDeleteField = EndDeleteField;
+
+	EndDeleteField = function(fieldId) {
+		var field = GetFieldById(fieldId);
+
+		if (field.type == 'repeater') {
+			var repeaterEndId = gfRepeater_getRepeaterEndId(fieldId);
+		}
+
+		og_EndDeleteField.call(this, fieldId);
+
+		if (field.type == 'repeater' && repeaterEndId) {
+			repeaterEndId = gfRepeater_getId(repeaterEndId);
+			DeleteField(repeaterEndId);
+		}
+	};
+}
+
+function gfRepeater_editforms_patch_SortUpdate() {
+	jQuery('#gform_fields').sortable({
+		update: function (event, ui) {
+			if (gfRepeater_brokenRepeater()) {
+				jQuery('#gform_fields').sortable('cancel');
+			}
+		}
+	});
 }
 
 jQuery(document).ready(function($) {
 	if (gfRepeater_page == 'gf_edit_forms') {
 		gfRepeater_editforms_update(false);
-		gfRepeater_editforms_updateUpdateButton();
+		jQuery('#add_fields input[type=button][data-type=repeater-end]').remove();
+	}
+});
+
+jQuery(window).load(function() {
+	if (gfRepeater_page == 'gf_edit_forms') {
+		gfRepeater_editforms_patch_SaveForm();
+		gfRepeater_editforms_patch_StartAddField();
+		gfRepeater_editforms_patch_EndAddField();
+		gfRepeater_editforms_patch_StartDeleteField();
+		gfRepeater_editforms_patch_EndDeleteField();
+		gfRepeater_editforms_patch_SortUpdate();
 	}
 });
 
